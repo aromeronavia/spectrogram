cd9d95c33d67a5962f0de770e3df6be7b2f2c072
aromeronavia <alberto@neurelo.com>
2023-09-13 10:22:21 -0600

    Yes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use hound;
 use rustfft::algorithm::Radix4;
 use rustfft::num_complex::Complex;
@@ -16,16 +18,36 @@ fn main() {
     let overlap = frame_size / 2;
     let fft = Radix4::new(frame_size, FftDirection::Forward);
 
-    for (_i, frame) in samples.windows(frame_size).step_by(overlap).enumerate() {
-        let mut frame: Vec<Complex<f32>> = frame.iter().enumerate().map(|(j, &s)| {
-            let window = 0.54 - 0.46 * (2.0 * std::f32::consts::PI * j as f32 / (frame_size as f32 - 1.0)).cos();
-            Complex::new(s * window, 0.0)
-        }).collect();
+    let img_width = frame_size / 2;
+    let img_height = samples.len() / frame_size;
+    let mut img = ImageBuffer::new(img_width as u32, img_height as u32);
 
-        fft.process(&mut frame);
+    for (i, frame) in samples.windows(frame_size).step_by(overlap).enumerate() {
+        if i >= img_height {
+            break;
+        }
 
-        for (j, value) in frame.iter().enumerate().take(img_width) {
-            fasd
+        let mut frame2: Vec<Complex<f32>> = frame
+            .iter()
+            .enumerate()
+            .map(|(j, &s)| {
+                let window = 0.54
+                    - 0.46
+                        * (2.0 * std::f32::consts::PI * j as f32 / (frame_size as f32 - 1.0)).cos();
+                Complex::new(s * window, 0.0)
+            })
+            .collect();
+
+        fft.process(&mut frame2);
+
+        for (j, value) in frame2.iter().enumerate().take(img_width) {
+            if j >= img_width {
+                break;
+            }
+            let magnitude = (value.norm() * 255.0) as u8;
+            img.put_pixel(j as u32, i as u32, Rgb([magnitude, magnitude, magnitude]));
         }
     }
+
+    img.save(Path::new("spectrogram.png")).unwrap();
 }
